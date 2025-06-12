03bf89158a4fb705d708b8efd924073fffecfc53
Valentin Brebion <v.brebion@registresecurite.com>
2025-06-12 09:56:21 +0200

    ss

--- docs/public/admin/admin.js
+++ docs/public/admin/admin.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 const supabaseUrl = 'https://efyehoznpzaaqelgxibl.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmeWVob3pucHphYXFlbGd4aWJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MTA1NjgsImV4cCI6MjA2NTI4NjU2OH0.RyE6U1frOhI3YDCpeV2OL_6WS7wyQT9DCgaAIUecbPg';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
